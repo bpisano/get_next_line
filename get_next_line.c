e20b69fc3fb4752b5eb824d09d7d1fa25de13cd9
Benjamin Pisano <benjamin.pisano@icloud.com>
2017-12-07 13:30:13 +0100

    get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.c                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2017/12/01 11:52:58 by bpisano      #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/07 13:27:36 by bpisano     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int		n_index(const char *str)
+{
+	int		i;
+
+	if (!str)
+		return (-1);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static t_list	*current_file(int fd, t_list **file_lst)
+{
+	t_list	*current;
+
+	if (!*file_lst)
+	{
+		*file_lst = ft_lstnew(NULL, 0);
+		(*file_lst)->content_size = (size_t)fd;
+		return (*file_lst);
+	}
+	current = *file_lst;
+	while (current)
+	{
+		if (current->content_size == (size_t)fd)
+			return (current);
+		current = current->next;
+	}
+	if (!current)
+	{
+		current = ft_lstnew(NULL, 0);
+		current->content_size = (size_t)fd;
+		ft_lstadd(file_lst, current);
+	}
+	return (current);
+}
+
+static int		read_rest(char **rest, char **line)
+{
+	int		n;
+
+	if ((n = n_index(*rest)) == -1)
+		return (0);
+	*line = ft_strsub(*rest, 0, n);
+	*rest += n + 1;
+	return (1);
+}
+
+static int		read_buff(int fd, char **rest)
+{
+	char			buff[BUFF_SIZE + 1];
+	char			*tmp;
+	int				char_count;
+	int				n;
+	int				r;
+
+	r = 0;
+	while ((char_count = read(fd, buff, BUFF_SIZE)))
+	{
+		r = 1;
+		buff[char_count] = '\0';
+		tmp = *rest;
+		*rest = ft_strjoin(tmp, buff);
+		free(tmp);
+		if ((n = n_index(*rest)) != -1)
+			break ;
+	}
+	return (r);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_list	*file_lst;
+	t_list			*file;
+	int				r;
+
+	if (!line || BUFF_SIZE < 1 || read(fd, NULL, 0) == -1)
+		return (-1);
+	file = current_file(fd, &file_lst);
+	if (read_rest((char **)&(file->content), line))
+		return (1);
+	if (!file->content)
+		file->content = ft_strdup("");
+	else
+		file->content = ft_strdup((char *)(file->content));
+	r = read_buff(fd, (char **)&(file->content));
+	if (read_rest((char **)&(file->content), line))
+		return (1);
+	*line = (char *)(file->content);
+	return (r);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,46 +1,30 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   main.c                                           .::    .:/ .      .::   */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2017/12/01 12:16:30 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/06 19:58:25 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Created: 2017/12/01 11:53:15 by bpisano      #+#   ##    ##    #+#       */
+/*   Updated: 2017/12/06 19:40:02 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int		get_fd(char *file)
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 3
+
+typedef struct		s_fd
 {
-	return (open(file, O_RDONLY));
-}
+	int				fd;
+	char			*rest;
+	struct s_fd		*next;
+}					t_fd;
 
-int		main(int ac, char **av)
-{
-	int		i;
-	int		fd;
-	char	*line;
-
-	if (ac == 2)
-	{
-		fd = get_fd(av[1]);
-		while (get_next_line(fd, &line) == 1)
-			printf("%s\n", line);
-		printf("Final : %s\n", line);
-		return (0);
-	}
-	i = 1;
-	while (i < ac)
-	{
-		fd = get_fd(av[i]);
-		while (get_next_line(fd, &line) == 1)
-			printf("%s", line);
-		i++;
-	}
-	return (0);
-}
+int					get_next_line(const int fd, char **line);
+#endif
